577aa2384d5f6094bcdccf22f50545d782718850
Javier André Hernández Hernández <andrehdz@MacBook-Pro-de-Javier-2.local>
2024-10-09 11:38:49 -0600

    cambios en SEO

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'PVR Legal';
-export const SITE_DESCRIPTION = 'Abogado especializado en derecho inmobiliario en Puerto Vallarta, Jalisco, México.';
+export const SITE_DESCRIPTION = 'Real estate lawyer in Puerto Vallarta, Jalisco, Mexico.';
